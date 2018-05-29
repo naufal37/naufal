799391576c7dfa0eeadc3522c2e75d207346c248
nau37 <supernaufalboy@gmail.com>
2018-05-29 16:28:48 +0700

    p19 checkpoint 6 Setup Static Files CSS & javascript

--- p19/urls.py
+++ p19/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^post/', include("posts.urls",namespace="post")),
 
     # url(r'^posts/$', 'posts.views.posts_list'),
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
