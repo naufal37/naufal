3f6226931a5e9fd4e34d1e9e14fb8c379df1df67
nau37 <supernaufalboy@gmail.com>
2018-06-25 17:04:33 +0700

    p19 checkpoint 10 & 11 (Slug field & Share Link ) V2

--- posts/models.py
+++ posts/models.py
@@ -3,15 +3,24 @@ from django.db import models
 from django.core.urlresolvers import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 
 def upload_location(instance,filename) :
     return "%s/%s" %(instance.id,filename)
 
 
+class PostManager(models.Manager) :
+    def active(self,*args,**kwargs):
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
 class Post(models.Model) :
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, default=1)
     title = models.CharField(max_length=120)
     image = models.ImageField(null=True,blank=True,width_field="width_field",height_field="height_field")
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     slug = models.SlugField(unique=True)
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
@@ -19,6 +28,7 @@ class Post(models.Model) :
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    objects = PostManager()
 
     def __str__(self):
         return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,19 @@
 from urllib.parse import quote_plus
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect , Http404
+from django.template.defaultfilters import title
+from django.db.models import Q
+
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from django.utils import timezone
 # Create your views here.
 
 def posts_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -21,6 +26,9 @@ def posts_create(request):
 
 def posts_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
+    if instance.draft or instance.publish > timezone.now().date() :
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
     context = {
         "title": instance.title,
@@ -29,7 +37,18 @@ def posts_detail(request, slug=None):
     return render(request, "list.html", context)
 
 def posts_list(request):
-    queryset_list = Post.objects.all()
+    today = timezone.now().date()
+    queryset_list = Post.objects.active()
+    if request.user.is_staff or request.user.is_superuser :
+        queryset_list = Post.objects.all()
+    query = request.GET.get("q")
+    if query :
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query)|
+            Q(content__icontains=query)|
+            Q(user__first_name__icontains=query)|
+            Q(user__last_name__icontains=query)
+        ).distinct()
     paginator = Paginator(queryset_list, 10)  # Show 25 contacts per page
     page_request_var = "halaman"
     page = request.GET.get(page_request_var)
@@ -44,16 +63,20 @@ def posts_list(request):
     context = {
         "object_list" : queryset,
         "title": "List",
-        "page_request_var" : page_request_var}
+        "page_request_var" : page_request_var,
+        "today": today,}
 
     return render(request,"post_list.html",context)
 
 def posts_update(request,slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None,request.FILES or None,instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        instance.user = request.user
         messages.success(request, "succed",extra_tags='some-tag')
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
@@ -64,6 +87,8 @@ def posts_update(request,slug=None):
 
 
 def posts_delete(request,slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "succed deleted", extra_tags='some-tag')
